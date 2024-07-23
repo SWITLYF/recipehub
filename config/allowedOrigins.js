04c137daf8788992ff7ec2cb4e2cb6b78701b4bf
Dadieng <alfreddadieng18@outlook.com>
2024-07-23 15:34:45 +0100

    update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "http://localhost:5000",
+  "https://recipehub.com.ng",
+  "https://recipehub-frontend.vercel.app",
   "https://recipen.vercel.app",
   "https://recipen-backend.onrender.com",
 ];
