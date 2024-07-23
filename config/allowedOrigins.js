69f114e8c53cc16fb129519774e5da19642db251
Dadieng <alfreddadieng18@outlook.com>
2024-07-23 14:46:25 +0100

    api update

---
+++ config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "http://localhost:5000",
+  "https://recipen.vercel.app",
+  "https://recipen-backend.onrender.com",
+];
+
+module.exports = allowedOrigins;
